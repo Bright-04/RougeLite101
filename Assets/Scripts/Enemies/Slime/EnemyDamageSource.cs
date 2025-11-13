16ca4f7a6d5c9c836f634c17aa07a9148c405dc1
Bright <mastereziono1@gmail.com>
2025-11-13 17:13:41 +0700

    Add knockback and flash effects to player on enemy hit
    Enhanced EnemyDamageSource to apply knockback and trigger a flash effect on the player when damaged. Updated Dungeon.unity scene to add Knockback and Flash components to the Player GameObject.

--- Assets/Scripts/Enemies/Slime/EnemyDamageSource.cs
+++ Assets/Scripts/Enemies/Slime/EnemyDamageSource.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using System.Collections;
 
 public class EnemyDamageSource : MonoBehaviour
 {
     [SerializeField] private float damageAmount = 3;
     [SerializeField] private float damageCooldown = 0.5f;
+    [SerializeField] private float knockbackForce = 10f; // Force applied to player
     private float damageTimer = 0;
 
     private void Update()
@@ -16,8 +18,21 @@ public class EnemyDamageSource : MonoBehaviour
     {
         if (other.TryGetComponent(out PlayerStats playerStats) && damageTimer <= 0)
         {
+            // Deal damage
             playerStats.TakeDamage(damageAmount);
             damageTimer = damageCooldown;
+            
+            // Apply knockback to player
+            if (other.TryGetComponent(out Knockback playerKnockback))
+            {
+                playerKnockback.GetKnockedBack(transform, knockbackForce);
+            }
+            
+            // Flash player red
+            if (other.TryGetComponent(out Flash playerFlash))
+            {
+                StartCoroutine(playerFlash.FlashRoutine());
+            }
         }
     }
 
